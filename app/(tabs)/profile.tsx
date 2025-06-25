ce701d6d91580cbeacf4adb12e8b226ac4469947
Dieegaso <dieegaso4@gmail.com>
2025-06-24 23:34:19 -0500

    second commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+        <Image/>
+    </View>
+  );
+};
+
+
+export default profile;
